c827d64b84a6644bc60dda861985f46d3ca5de64
Raquel Freitas <raquelsfreitasmaia@gmail.com>
2018-12-04 10:29:20 -0200

    Grafico Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,26 +24,28 @@ export class DashboardComponent implements OnInit{
             .subscribe(
                 data => this.dashItens = data
             );
-console.log(this.dashItens);
+
+
         var dataSales = {
           labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
           series: [
-             [287, 385, 490, 562, 594, 626, 698, 895, 952],
-            [67, 152, 193, 240, 387, 435, 535, 642, 744],
-            [23, 113, 67, 108, 190, 239, 307, 410, 410]
+             [4, 60, 9, 30, 40, 50, 10, 1, 1],
+             [20, 10, 3, 7, 22, 20, 7, 8, 1],
+             [1, 5, 3, 9, 5, 4, 7, 6, 3],
+             [1, 4, 3, 4, 1, 2, 7, 20, 10],
           ]
         };
 
         var optionsSales = {
           low: 0,
-          high: 1000,
+          high: 100,
           showArea: true,
           height: "245px",
           axisX: {
             showGrid: false,
           },
           lineSmooth: Chartist.Interpolation.simple({
-            divisor: 3
+            divisor: 4
           }),
           showLine: true,
           showPoint: false,
@@ -61,58 +63,5 @@ console.log(this.dashItens);
 
         new Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
 
-
-        var data = {
-          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-          series: [
-            [542, 543, 520, 680, 653, 753, 326, 434, 568, 610, 756, 895],
-            [230, 293, 380, 480, 503, 553, 600, 664, 698, 710, 736, 795]
-          ]
-        };
-
-        var options = {
-            seriesBarDistance: 10,
-            axisX: {
-                showGrid: false
-            },
-            height: "245px"
-        };
-
-        var responsiveOptions: any[] = [
-          ['screen and (max-width: 640px)', {
-            seriesBarDistance: 5,
-            axisX: {
-              labelInterpolationFnc: function (value) {
-                return value[0];
-              }
-            }
-          }]
-        ];
-
-        new Chartist.Line('#chartActivity', data, options, responsiveOptions);
-
-        var dataPreferences = {
-            series: [
-                [25, 30, 20, 25]
-            ]
-        };
-
-        var optionsPreferences = {
-            donut: true,
-            donutWidth: 40,
-            startAngle: 0,
-            total: 100,
-            showLabel: false,
-            axisX: {
-                showGrid: false
-            }
-        };
-
-        new Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
-
-        new Chartist.Pie('#chartPreferences', {
-          labels: ['62%','32%','6%'],
-          series: [62, 32, 6]
-        });
     }
 }
